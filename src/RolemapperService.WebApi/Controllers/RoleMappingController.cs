c001e18e32740a0430a6b342ca404f7862acec71
Jakob Nielsen <jakob.nielsen@dfds.com>
2018-12-12 14:49:24 +0100

    Added role mapping controller.

---
+++ src/RolemapperService.WebApi/Controllers/RoleMappingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RolemapperService.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoleMappingController : ControllerBase
+    {
+        [HttpGet("")]
+        public ActionResult<string> Map()
+        {
+            return Ok("OK");
+        }
+    }
+}
