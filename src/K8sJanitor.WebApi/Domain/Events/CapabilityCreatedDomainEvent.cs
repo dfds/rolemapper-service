c44561c6507b9270b6452cf152f88e8118ec4fec
Kim Lindhard <kilin@dfds.com>
2019-05-07 06:33:28 +0200

    add contextId, contextName to CapabilityCreatedDomainEvent

--- src/K8sJanitor.WebApi/Domain/Events/CapabilityCreatedDomainEvent.cs
+++ src/K8sJanitor.WebApi/Domain/Events/CapabilityCreatedDomainEvent.cs
@@ -21,13 +21,22 @@ namespace K8sJanitor.WebApi.Domain.Events
 
     public class CapabilityCreatedData
     {
-        public Guid CapabilityId { get; private set; }
-        public string CapabilityName { get; private set; }
-
-        public CapabilityCreatedData(Guid capabilityId, string capabilityName)
+        public CapabilityCreatedData(
+            Guid capabilityId, 
+            string capabilityName,
+            Guid contextId, 
+            string contextName
+        )
         {
             CapabilityId = capabilityId;
             CapabilityName = capabilityName;
+            ContextId = contextId;
+            ContextName = contextName;
         }
+        public Guid CapabilityId { get; }
+        
+        public string CapabilityName { get; }
+        public Guid ContextId { get; }
+        public string ContextName { get; }
     }
 }
