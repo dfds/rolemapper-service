3d3514d9a9e0328f0674196307c7cde0c7616a00
Emil H. Clausen <emcla@dfds.com>
2019-06-27 11:12:23 +0200

    Removed code that was deemed not mission critical
    * The implementation from CapabilityService that this is based on, included DB fallback as well as various decorators

--- local-development/watch-run.sh
+++ local-development/watch-run.sh
@@ -3,5 +3,4 @@
 KUBERNETES_SERVICE_KAFKA_BOOTSTRAP_SERVERS=localhost:9092 \
 KUBERNETES_SERVICE_KAFKA_GROUP_ID=kubernetes-consumer \
 KUBERNETES_SERVICE_KAFKA_ENABLE_AUTO_COMMIT=false \
-K8SJANITOR_SERVICE_DATABASE_CONNECTIONSTRING="User ID=postgres;Password=p;Host=localhost;Port=5432;Database=k8sjanitordb;" \
 dotnet watch --project ./../src/K8sJanitor.WebApi/K8sJanitor.WebApi.csproj run
