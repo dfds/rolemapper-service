d2e2e0cd6d04a8db603cc02cca6c1aac560009bc
Emil H. Clausen <emcla@dfds.com>
2019-06-27 13:52:00 +0200

    Events to be published stored on its own. Added k8s_namespace_created_and_aws_arn_connected to K8sApplicationService

--- src/K8sJanitor.WebApi/Application/K8sApplicationService.cs
+++ src/K8sJanitor.WebApi/Application/K8sApplicationService.cs
@@ -1,13 +1,42 @@
+using System;
 using System.Threading.Tasks;
+using K8sJanitor.WebApi.Domain.Events;
+using K8sJanitor.WebApi.Infrastructure.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace K8sJanitor.WebApi.Application
 {
     public class K8sApplicationService : IK8sApplicationService
     {
+        private readonly IServiceProvider _serviceProvider;
 
-        public K8sApplicationService()
+        public K8sApplicationService(IServiceProvider serviceProvider)
         {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task FireEventK8sNamespaceCreatedAndAwsArnConnected(string namespaceName, Guid contextId)
+        {
+            var eventRegistry = _serviceProvider.GetRequiredService<DomainEventRegistry>();
+            var eventsQueue = _serviceProvider.GetRequiredService<PublishingEventsQueue>();
+
+            var evtPre = new K8sNamespaceCreatedAndAwsArnConnectedEvent(namespaceName, contextId);
             
+            var evt = new DomainEventEnvelope
+            {
+                EventId = Guid.NewGuid(),
+                Created = DateTime.UtcNow,
+                Type = eventRegistry.GetTypeNameFor(evtPre),
+                Format = "application/json",
+                Data = JsonConvert.SerializeObject(evtPre, new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                })
+            };
+            
+            eventsQueue.AddEventToQueue(evt);
         }
         
         public Task TestCreated(string description)
--- src/K8sJanitor.WebApi/Infrastructure/Messaging/PublishingService.cs
+++ src/K8sJanitor.WebApi/Infrastructure/Messaging/PublishingService.cs
@@ -1,15 +1,9 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using K8sJanitor.WebApi.Domain.Events;
+using K8sJanitor.WebApi.Application;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Newtonsoft.Json.Serialization;
 using Serilog;
 
 namespace K8sJanitor.WebApi.Infrastructure.Messaging
@@ -17,25 +11,12 @@ namespace K8sJanitor.WebApi.Infrastructure.Messaging
     public class PublishingService : BackgroundService
     {
         private readonly IServiceProvider _serviceProvider;
-        private Queue<DomainEventEnvelope> _eventsToBeSent;
+        private PublishingEventsQueue _eventsQueue;
 
         public PublishingService(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
-            _eventsToBeSent = new Queue<DomainEventEnvelope>();
-        }
-
-        public void AddEventToQueue(DomainEventEnvelope iEvent)
-        {
-            _eventsToBeSent.Enqueue(iEvent);
-        }
-
-        public void AddEventsToQueue(IEnumerable<DomainEventEnvelope> events)
-        {
-            foreach (DomainEventEnvelope @event in events)
-            {
-                _eventsToBeSent.Enqueue(@event);
-            }
+            _eventsQueue = _serviceProvider.GetService<PublishingEventsQueue>();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -46,7 +27,10 @@ namespace K8sJanitor.WebApi.Infrastructure.Messaging
             {
                 try
                 {
-                    await DoTestEvent(stoppingToken); // Used for learning purposes. REMOVE BEFORE MERGING TO MASTER
+                    // Used for learning purposes. REMOVE BEFORE MERGING TO MASTER
+                    var k8sapp = _serviceProvider.GetRequiredService<K8sApplicationService>();
+                    await k8sapp.FireEventK8sNamespaceCreatedAndAwsArnConnected("onprem-43581", Guid.NewGuid());
+                    // Stops here
                     await DoWork(stoppingToken);
                 }
                 catch (Exception err)
@@ -57,51 +41,18 @@ namespace K8sJanitor.WebApi.Infrastructure.Messaging
                 await Task.Delay(TimeSpan.FromSeconds(20), stoppingToken);
             }
         }
-
-        // Entirely for learning purposes. 
-        // REMOVE BEFORE MERGING TO MASTER.
-        private async Task DoTestEvent(CancellationToken stoppingToken)
-        {
-            Log.Information("Sending test event. If you see this message in PROD, this means something has gone live that shouldn't've");
-            var eventRegistry = _serviceProvider.GetRequiredService<DomainEventRegistry>();
-
-            var evtGeneral = new GeneralDomainEvent("1", "x", Guid.Empty, "", new K8sNamespaceCreatedAndAwsArnConnectEventData("An event to test if everything is configured properly"));
-            Log.Information($"evtGeneral payload: {evtGeneral.Payload}");
-            var evtPre = new K8sNamespaceCreatedAndAwsArnConnectedEvent("onprem-43581", Guid.Empty);
-
-            // AggregateId missing due to IAggregateDomainEvents not being used.
-            var evt = new DomainEventEnvelope
-            {
-                EventId = Guid.NewGuid(),
-                Created = DateTime.UtcNow,
-                Type = eventRegistry.GetTypeNameFor(evtPre),
-                Format = "application/json",
-                Data = JsonConvert.SerializeObject(evtPre, new JsonSerializerSettings
-                {
-                    ContractResolver = new CamelCasePropertyNamesContractResolver()
-                })
-            };
-            
-            this.AddEventToQueue(evt);
-        }
-
+        
         private async Task DoWork(CancellationToken stoppingToken)
         {
             using (var scope = _serviceProvider.CreateScope())
             {
-                // Entirely for learning purposes. 
-                // REMOVE BEFORE MERGING TO MASTER.
-                Log.Information("Check if there's any events to publish");
-                
-                // New way, minimal abstractions.
-
                 {
-                    if (_eventsToBeSent.Any() == false)
+                    if (_eventsQueue.AnyEventInQueue() == false)
                     {
                         return;
                     }
                 
-                    Log.Information($"Domain events to publish: {_eventsToBeSent.Count}");
+                    Log.Information($"Domain events to publish: {_eventsQueue.QueueCount()}");
                 
                     var publisherFactory = scope.ServiceProvider.GetRequiredService<KafkaPublisherFactory>();
                     var eventRegistry = scope.ServiceProvider.GetRequiredService<DomainEventRegistry>();
@@ -112,9 +63,9 @@ namespace K8sJanitor.WebApi.Infrastructure.Messaging
                     {
                         Log.Information("Connected!");
 
-                        while (_eventsToBeSent.Any())
+                        while (_eventsQueue.AnyEventInQueue())
                         {
-                            var evt = _eventsToBeSent.Dequeue();
+                            var evt = _eventsQueue.Dequeue();
                             var topicName = eventRegistry.GetTopicFor(evt.Type);
                             var message = MessagingHelper.CreateMessageFrom(evt);
 
