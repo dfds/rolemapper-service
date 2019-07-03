e82b3e4e7b747b0d0e021ec9500fd09efc8f7740
Emil H. Clausen <emcla@dfds.com>
2019-07-03 20:56:59 +0200

    Wrap event publish in a try-catch in order to ensure anything erroneous gets caught

--- src/K8sJanitor.WebApi/Application/K8sApplicationService.cs
+++ src/K8sJanitor.WebApi/Application/K8sApplicationService.cs
@@ -5,6 +5,7 @@ using K8sJanitor.WebApi.Infrastructure.Messaging;
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Serilog;
 
 namespace K8sJanitor.WebApi.Application
 {
@@ -19,24 +20,31 @@ namespace K8sJanitor.WebApi.Application
 
         public async Task FireEventK8sNamespaceCreatedAndAwsArnConnected(string namespaceName, Guid contextId)
         {
-            var eventRegistry = _serviceProvider.GetRequiredService<DomainEventRegistry>();
-            var eventsQueue = _serviceProvider.GetRequiredService<PublishingEventsQueue>();
-
-            var evtPre = new K8sNamespaceCreatedAndAwsArnConnectedEvent(namespaceName, contextId);
-            
-            var evt = new DomainEventEnvelope
+            try
             {
-                EventId = Guid.NewGuid(),
-                Created = DateTime.UtcNow,
-                Type = eventRegistry.GetTypeNameFor(evtPre),
-                Format = "application/json",
-                Data = JsonConvert.SerializeObject(evtPre, new JsonSerializerSettings
-                {
-                    ContractResolver = new CamelCasePropertyNamesContractResolver()
-                })
-            };
+                var eventRegistry = _serviceProvider.GetRequiredService<DomainEventRegistry>();
+                var eventsQueue = _serviceProvider.GetRequiredService<PublishingEventsQueue>();
+
+                var evtPre = new K8sNamespaceCreatedAndAwsArnConnectedEvent(namespaceName, contextId);
             
-            eventsQueue.AddEventToQueue(evt);
+                var evt = new DomainEventEnvelope
+                {
+                    EventId = Guid.NewGuid(),
+                    Created = DateTime.UtcNow,
+                    Type = eventRegistry.GetTypeNameFor(evtPre),
+                    Format = "application/json",
+                    Data = JsonConvert.SerializeObject(evtPre, new JsonSerializerSettings
+                    {
+                        ContractResolver = new CamelCasePropertyNamesContractResolver()
+                    })
+                };
+            
+                eventsQueue.AddEventToQueue(evt);
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"An error occured trying to emit an event from K8sApplicationService: {ex.Message}");
+            }
         }
     }
 }
