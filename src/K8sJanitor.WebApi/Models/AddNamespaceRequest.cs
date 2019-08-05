2616c4cf69e67d1d8ae0a640420ef9fbf96fa61c
JacobHeidelbachDFDS <jfhei@dfds.com>
2019-08-05 15:49:49 +0200

    Updated based on PR comment

--- src/K8sJanitor.WebApi/Models/AddNamespaceRequest.cs
+++ src/K8sJanitor.WebApi/Models/AddNamespaceRequest.cs
@@ -3,6 +3,6 @@ namespace K8sJanitor.WebApi.Models
     public class AddNamespaceRequest
     {
         public string NamespaceName { get; set; }
-        public string RoleName { get; set; }
+        public string AccountId { get; set; }
     }
 }
