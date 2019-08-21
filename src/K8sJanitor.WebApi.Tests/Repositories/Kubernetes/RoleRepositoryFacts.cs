befb485a73437d146a710d1942050e0cfaf6d18e
Willi Carlsen <carlsenwilli@gmail.com>
2019-08-21 15:28:25 +0200

    Forgot to save.... damn

--- src/K8sJanitor.WebApi.Tests/Repositories/Kubernetes/RoleRepositoryFacts.cs
+++ src/K8sJanitor.WebApi.Tests/Repositories/Kubernetes/RoleRepositoryFacts.cs
@@ -49,16 +49,11 @@ namespace K8sJanitor.WebApi.Tests.Repositories.Kubernetes
 
             Mock.Get(k8s).Setup(k => k.CreateNamespacedRoleAsync(It.IsAny<V1Role>(),
                     It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
-<<<<<<< HEAD
                 .Throws(new HttpOperationException
                 {
                     Response =
                     new HttpResponseMessageWrapper(new HttpResponseMessage { StatusCode = HttpStatusCode.Conflict }, "")
                 });
-=======
-                .Throws(new HttpOperationException{ Response =
-                    new HttpResponseMessageWrapper(new HttpResponseMessage{StatusCode = HttpStatusCode.Conflict},"")});
->>>>>>> Altering tests to match changes
 
             var @namespace = "fancyNamespace";
 
@@ -76,16 +71,11 @@ namespace K8sJanitor.WebApi.Tests.Repositories.Kubernetes
 
             Mock.Get(k8s).Setup(k => k.CreateNamespacedRoleAsync(It.IsAny<V1Role>(),
                     It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
-<<<<<<< HEAD
                 .Throws(new HttpOperationException
                 {
                     Response =
                     new HttpResponseMessageWrapper(new HttpResponseMessage { StatusCode = HttpStatusCode.BadGateway }, "Unable to communicate")
                 });
-=======
-                .Throws(new HttpOperationException{ Response =
-                    new HttpResponseMessageWrapper(new HttpResponseMessage{StatusCode = HttpStatusCode.BadGateway},"Unable to communicate")});
->>>>>>> Altering tests to match changes
 
             var @namespace = "fancyNamespace";
 
@@ -93,11 +83,7 @@ namespace K8sJanitor.WebApi.Tests.Repositories.Kubernetes
 
         }
 
-<<<<<<< HEAD
     }
-=======
-}
->>>>>>> Altering tests to match changes
 
 
 }
