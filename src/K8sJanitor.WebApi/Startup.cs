062a4d06cba4fab0855b18ac37a5fe3e572e09da
toban@dfds.com <toban@dfds.com>
2020-04-20 11:32:10 +0200

    - Upgraded to .net core 3.1

--- src/K8sJanitor.WebApi/Startup.cs
+++ src/K8sJanitor.WebApi/Startup.cs
@@ -26,15 +26,15 @@ using K8sJanitor.WebApi.Repositories.Kubernetes;
 using K8sJanitor.WebApi.Services;
 using K8sJanitor.WebApi.Validators;
 using K8sJanitor.WebApi.Wrappers;
-using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+using Microsoft.AspNetCore.Hosting;
 
 namespace K8sJanitor.WebApi
 {
     public class Startup
     {
-        private readonly IHostingEnvironment _hostingEnvironment;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public Startup(IConfiguration configuration, IHostingEnvironment hostingEnvironment)
+        public Startup(IConfiguration configuration, IWebHostEnvironment hostingEnvironment)
         {
             Configuration = configuration;
             _hostingEnvironment = hostingEnvironment;
@@ -76,9 +76,8 @@ namespace K8sJanitor.WebApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
-
+            services.AddMvc((options) => options.EnableEndpointRouting = false).SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            
             if (
                 string.IsNullOrWhiteSpace(Configuration["KUBERNETES_SERVICE_HOST"]) == false &&
                 string.IsNullOrWhiteSpace(Configuration["KUBERNETES_SERVICE_PORT"]) == false
@@ -166,7 +165,7 @@ namespace K8sJanitor.WebApi
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
